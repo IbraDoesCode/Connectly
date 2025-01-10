68c5e5d4f253b1bf438a31018ef491f5287218d4
Kristopher Santos <topers1820@gmail.com>
2025-01-10 11:12:44 +0800

    Implemented base CRUD
    - Added Base Model class
    - Added model classes for Users and Posts
    - Added Views for creating and getting the models
    - Setup urls and migration

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
+class BaseModel(models.Model):
+    objects = models.Manager()
+    DoesNotExist = models.ObjectDoesNotExist
+
+    class Meta:
+        abstract = True
+
+
 # Create your models here.
+class User(BaseModel):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Post(BaseModel):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:50]
