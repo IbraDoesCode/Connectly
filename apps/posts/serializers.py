0586bd2ce9d1a64d245e6fafe04a138f88f9de9b
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-01-16 20:58:23 +0800

    Create serializers for Post and Comment models

---
+++ apps/posts/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Post, Comment
+from ..users.models import User
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'text', 'author', 'post', 'created_at']
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'content', 'author', 'created_at', 'comments']
