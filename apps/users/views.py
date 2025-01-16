df6627d6ded5313e124fbcda1b6d1b1d1713566b
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-01-16 21:03:54 +0800

    Create class based views for Users app.
    - Logic Implementation pending.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
+
+class UserListView(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class UserDetailView(APIView):
+    def get(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
