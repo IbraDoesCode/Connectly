c7f433a23385311dc941313059c576492dc5a4f7
Morfusee <markvalenzuela.coding@gmail.com>
2025-01-18 13:33:03 +0800

    FIX (Posts): Correct URL Pattern in Posts app

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import PostListView, PostDetailView, CommentListView, CommentDetailView
 
 urlpatterns = [
-    path('posts/', PostListView.as_view(), name='post_list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:post_id>/comments/', CommentListView.as_view(), name='comment_list'),
-    path('posts/<int:post_id>/comments/<int:comment_id>/', CommentDetailView.as_view(), name='comment_detail'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('<int:post_id>/comments/', CommentListView.as_view(), name='comment_list'),
+    path('<int:post_id>/comments/<int:comment_id>/', CommentDetailView.as_view(), name='comment_detail'),
 ]
