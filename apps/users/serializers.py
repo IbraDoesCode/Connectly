838783af5000d8d171e3b3d06ed6d83b2e988756
Harvey Dela Flor <lr.hdflor@mmdc.mcl.edu.ph>
2025-01-20 20:21:16 +0800

    Updated UserSerializers
    Limited included fields for better abstraction upon updating the model

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .models import User
-from ..posts.serializers import PostSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['username', 'email']  
