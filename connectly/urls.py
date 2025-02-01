b17583947dc70cf3f0578d16306b3b6746d496c9
Harvey Dela Flor <lr.hdflor@mmdc.mcl.edu.ph>
2025-02-01 13:49:01 +0800

    Updated Main Urls.py
    - used "token/" instead of "login/" for generating tokens (this uses JWT's TokenObtainPairView)
    - added logout view
    
    Note: use "token/refresh" passing the refresh token for generating new access token

--- connectly/urls.py
+++ connectly/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import  LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #This will be called by the cilent to refresh the token
     path('posts/', include('apps.posts.urls')),
-    path('users/', include('apps.users.urls'))
+    path('users/', include('apps.users.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), #This will be called by the client to refresh the token
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
