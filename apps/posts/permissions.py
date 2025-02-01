07d8d9ea0208de75ad91317e191d302bf9040ca7
Harvey Dela Flor <lr.hdflor@mmdc.mcl.edu.ph>
2025-02-01 12:24:23 +0800

    FIX: Updated IsAuthor
    Used author and user for comparison instead of their ID's. This secures ID mismatch.

--- apps/posts/permissions.py
+++ apps/posts/permissions.py
@@ -2,4 +2,4 @@ from rest_framework.permissions import BasePermission
 
 class IsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.author_id == request.user.id
+        return obj.author == request.user
