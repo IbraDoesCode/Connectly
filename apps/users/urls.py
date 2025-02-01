1a7b44ff570650238acaf406cac896eedf657804
Harvey Dela Flor <lr.hdflor@mmdc.mcl.edu.ph>
2025-02-01 13:46:07 +0800

    Updated User Views
    - used 'access' and 'refresh' for user registration view to match the variable names of JWT's response on generating tokens
    - removed UserLoginView since it's much better to implement JWT's built in token generation (TokenObtainPairView)

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import UserListView, UserRegistrationView, UserLoginView
+from .views import UserListView, UserRegistrationView
 
 urlpatterns = [
     path('', UserListView.as_view(), name='user_list'),
     path('register/', UserRegistrationView.as_view(), name='user-register'),
-    path('login/', UserLoginView.as_view(), name='user-login')
 ]
