80f558618ae1496b345e280f692a0c4a128c4438
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-02-18 18:37:49 +0800

    Add URL path for follow & unfollow

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('profile/<int:user_id>', ProfileByIDView.as_view(), name='user-profile-from-id'),
     path('profile/search/', ProfileSearchSuggestionsView.as_view(), name='profile-search-suggestions'), # * Usage: /users/profile/search/?search_query=*name*
     path('profile/comments/', PersonalCommentsView.as_view(), name='personal-comments'),
+    path('follow/', FollowView.as_view(), name='follow'),
+    path('unfollow/', UnfollowView.as_view(), name='unfollow'),
 ]
