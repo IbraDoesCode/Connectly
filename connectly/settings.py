af04944c07795e01b517cf3da1de22066a9f6f8a
Morfusee <markvalenzuela.coding@gmail.com>
2025-03-05 15:13:23 +0800

    UPDATE: Changed throttle rate for users to 10 per second

--- connectly/settings.py
+++ connectly/settings.py
@@ -99,7 +99,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '10/minute',
-        'user': '2/second',
+        'user': '10/second',
     },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
