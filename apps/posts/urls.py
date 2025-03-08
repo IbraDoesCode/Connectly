e315508deb7f969c2ac16c38ddcedbb530c57e60
Kristopher Santos <topers1820@gmail.com>
2025-03-08 15:35:46 +0800

    FEAT: Feed, Media Rework, and Follow Retrieval
    - Implemented personal feed
    - Reworked media to be more general and reusable
    - Changed media url
    - Implemented get method for followers/followings
    - Reworked profile urls
    - Moved the follow test to a separate file

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('<int:post_id>/', PostDetailView.as_view(), name='post_detail'),
     path('<int:post_id>/comments/', CommentListView.as_view(), name='comment_list'),
     path('<int:post_id>/comments/<int:comment_id>/', CommentDetailView.as_view(), name='comment_detail'),
-    path('<int:post_id>/like/', LikePostView.as_view(), name='like_post'),
-    path('<int:post_id>/comments/<int:comment_id>/like/', LikeCommentView.as_view(), name='like_comment')
+    path('<int:post_id>/like', LikePostView.as_view(), name='like_post'),
+    path('<int:post_id>/comments/<int:comment_id>/like', LikeCommentView.as_view(), name='like_comment')
 ] 
