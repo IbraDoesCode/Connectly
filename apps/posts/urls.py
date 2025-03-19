d5f55b75afdd6a160ef2bfd6086630cc2a996529
Kristopher Santos <topers1820@gmail.com>
2025-03-19 18:45:08 +0800

    Changes for Frontend Integration

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -7,7 +7,7 @@ from .views import *
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<int:post_id>/', PostDetailView.as_view(), name='post_detail'),
-    path('<int:post_id>/comments/', CommentListView.as_view(), name='comment_list'),
+    path('<int:post_id>/comments', CommentListView.as_view(), name='comment_list'),
     path('<int:post_id>/comments/<int:comment_id>/', CommentDetailView.as_view(), name='comment_detail'),
     path('<int:post_id>/like', LikePostView.as_view(), name='like_post'),
     path('<int:post_id>/comments/<int:comment_id>/like', LikeCommentView.as_view(), name='like_comment')
