d1fc889751ac98afeff107d0457e56ff41d5e0cb
Ibra <lr.idesoukyharby@mmdc.mcl.edu.ph>
2025-04-21 23:17:05 +0300

    feat: Add Cloudinary configuration in apps.py
    - Initialize Cloudinary with credentials from environment variables

--- apps/medias/apps.py
+++ apps/medias/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+import cloudinary
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class MediasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.medias'
+
+    def ready(self):    
+        cloudinary.config(
+            cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+            api_key=os.getenv('CLOUDINARY_API_KEY'),
+            api_secret=os.getenv('CLOUDINARY_API_SECRET')
+        )
